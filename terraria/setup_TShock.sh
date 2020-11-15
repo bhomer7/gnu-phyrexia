04cae7ee1db78c1c7a82c25a5d551f3676a1b178
Ben Homer <ben.homer@smartobjx.com>
2020-11-15 17:43:21 -0600

    Fix bad unzip filename

--- terraria/setup_TShock.sh
+++ terraria/setup_TShock.sh
@@ -5,7 +5,7 @@ cd TERRARIA_ROOT/..
 mkdir Release
 cd Release
 curl -LJO https://github.com/Pryaxis/TShock/releases/download/v4.4.0-pre15/TShock4.4.0_Pre15_Terraria1.4.1.2.zip
-unzip TShock.zip
+unzip TShock4.4.0_Pre15_Terraria1.4.1.2.zip
 cd TShock4.4.0_Pre15_Terraria1.4.1.2
 mono TerrariaServer.exe
 
