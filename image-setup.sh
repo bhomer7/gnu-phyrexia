1ec6e7012889f9fb84d33fa869de5face0c5ad82
Ben Homer <ben.homer@smartobjx.com>
2020-11-15 15:56:30 -0600

    Update setup with -y and modify user creation

--- image-setup.sh
+++ image-setup.sh
@@ -2,15 +2,13 @@
 
 # Update
 sudo yum update -y
-sudo yum install tmux
-sudo amazon-linux-extras install docker
+sudo yum install -y tmux
+sudo amazon-linux-extras -y install docker
 
 # Add bhomer7 user
 sudo useradd -G wheel bhomer7
-sudo su bhomer7
-mkdir -p ~/.ssh
-exit
+mkdir -p /home/bhomer7/.ssh
 sudo cp ~/.ssh/authorized_keys /home/bhomer7/.ssh/authorized_keys
-sudo chown bhomer7 /home/bhomer7/.ssh/*
+sudo chown -R bhomer7 /home/bhomer7/.ssh
 
 
