ea5ee4281e49a7ea294d70659c130e7f296a93b4
Ben Homer <ben.homer@smartobjx.com>
2020-11-15 16:01:01 -0600

    Add curl command and missing sudo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # gnu-phyrexia
 Docker images for server I run in AWS.
+
+To run:
+```
+curl -o setup.sh https://raw.githubusercontent.com/bhomer7/gnu-phyrexia/main/image-setup.sh && chmod +x setup.sh && ./setup.sh
+```
--- image-setup.sh
+++ image-setup.sh
@@ -2,12 +2,12 @@
 
 # Update
 sudo yum update -y
-sudo yum install -y tmux
+sudo yum install -y tmux git
 sudo amazon-linux-extras -y install docker
 
 # Add bhomer7 user
 sudo useradd -G wheel bhomer7
-mkdir -p /home/bhomer7/.ssh
+sudo mkdir -p /home/bhomer7/.ssh
 sudo cp ~/.ssh/authorized_keys /home/bhomer7/.ssh/authorized_keys
 sudo chown -R bhomer7 /home/bhomer7/.ssh
 
