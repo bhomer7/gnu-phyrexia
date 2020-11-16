3b1eb2adbb07bdf4ea9daf2d6c62c519f320f241
Ben Homer <ben.homer@smartobjx.com>
2020-11-15 18:00:32 -0600

    Remove terraria build, Fix terraria setup

--- terraria/setup_TShock.sh
+++ terraria/setup_TShock.sh
@@ -1,11 +1,7 @@
 #! /bin/bash
 
 mkdir -p $TERRARIA_ROOT
-cd TERRARIA_ROOT/..
-mkdir Release
-cd Release
+cd $TERRARIA_ROOT
 curl -LJO https://github.com/Pryaxis/TShock/releases/download/v4.4.0-pre15/TShock4.4.0_Pre15_Terraria1.4.1.2.zip
-unzip TShock4.4.0_Pre15_Terraria1.4.1.2.zip
-mv TShock4.4.0_Pre15_Terraria1.4.1.2 TShock
-
-
+mv TShock4.4.0_Pre15_Terraria1.4.1.2.zip TShock.zip
+unzip TShock.zip
