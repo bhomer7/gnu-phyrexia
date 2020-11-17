ddca2146f3fdf4ffce10cc7a1f75e38cf05d8ea3
Ben Homer <bhomer77@gmail.com>
2020-11-17 04:57:18 +0000

    Add Makefile, Fix path and name issues for terraria exe

--- terraria/setup_TShock.sh
+++ terraria/setup_TShock.sh
@@ -3,5 +3,5 @@
 mkdir -p $TERRARIA_ROOT
 cd $TERRARIA_ROOT
 curl -LJO https://github.com/Pryaxis/TShock/releases/download/v4.4.0-pre15/TShock4.4.0_Pre15_Terraria1.4.1.2.zip
-mv TShock4.4.0_Pre15_Terraria1.4.1.2.zip TShock.zip
-unzip TShock.zip
+unzip TShock4.4.0_Pre15_Terraria1.4.1.2.zip
+mv TShock4.4.0_Pre15_Terraria1.4.1.2 TShock
