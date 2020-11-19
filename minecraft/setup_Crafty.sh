e1751c47af57a94df1a3d5402cca9dca49fa34e9
Ben Homer <ben.homer@smartobjx.com>
2020-11-18 23:45:31 -0600

    Move crafty setup to separate file. Add patch to work in AWS

---
+++ minecraft/setup_Crafty.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# Install crafty
+cd /tmp
+git clone https://gitlab.com/crafty-controller/crafty-installer-linux.git
+cd crafty-installer-linux
+(printf "y\ny\ny\nmaster\ny\n") | ./install_crafty.sh
+
+# Patch crafty to work in AWS
+cd /var/opt/minecraft/crafty/craft-web/app/classes/
+sed -i 's/psutil.cpu_freq...0./1000/' multiserv.py
+sed -i 's/psutil.cpu_freq...2./3100/' multiserv.py
+
+cd /var/opt/minecraft/crafty
+
+# Setup service
+#systemctl enable crafty.service
