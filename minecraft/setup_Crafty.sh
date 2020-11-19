c081f530d6cc8c46c68f11d31905380e08641f9b
Ben Homer <bhomer77@gmail.com>
2020-11-19 05:50:41 +0000

    Fix typo

--- minecraft/setup_Crafty.sh
+++ minecraft/setup_Crafty.sh
@@ -7,7 +7,7 @@ cd crafty-installer-linux
 (printf "y\ny\ny\nmaster\ny\n") | ./install_crafty.sh
 
 # Patch crafty to work in AWS
-cd /var/opt/minecraft/crafty/craft-web/app/classes/
+cd /var/opt/minecraft/crafty/crafty-web/app/classes/
 sed -i 's/psutil.cpu_freq...0./1000/' multiserv.py
 sed -i 's/psutil.cpu_freq...2./3100/' multiserv.py
 
