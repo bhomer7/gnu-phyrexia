8b2d8f1fe60b60c50d8122164ba8151b57c87188
Ben Homer <ben.homer@smartobjx.com>
2020-11-20 23:51:24 -0600

    Update gdrive download, Fix typo

--- minecraft/setup_minecraft.sh
+++ minecraft/setup_minecraft.sh
@@ -2,6 +2,13 @@
 
 # Create server
 cd /var/opt/minecraft/
-curl -o server.zip https://drive.google.com/file/d/16Cfwzvr6JXSxZpcX3BY06kQ_RrPl5OR5/view?usp=sharing
+
+# GDrive download process per https://stackoverflow.com/a/48133859
+fileid="16Cfwzvr6JXSxZpcX3BY06kQ_RrPl5OR5"
+filename="server.zip"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
 unzip server.zip
-rm server.zip
+rm ${filename} ./cookie
+
